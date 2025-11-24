8dc41ab42c089fee5b957f724cd257fb11072109
tjm650 <tinotendajenya.org@gmail.com>
2025-11-24 12:17:45 +0200

    Create +not-found.tsx

---
+++ frontend/app/app/+not-found.tsx
@@ -0,0 +1,35 @@
+import { Link, Stack } from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function NotFoundScreen() {
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: 'Oops!' }} />
+      <Text style={styles.title}>This screen doesn't exist.</Text>
+      <Link href="/" style={styles.link}>
+        <Text style={styles.linkText}>Go to home screen!</Text>
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+  linkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+});
