0f16df7f2ff1ada53af3c9cfc8de0649331afe7c
xtrm1002 <tinotendajenya.org@gmail.com>
2025-11-24 18:31:03 +0200

    changes 24/11/25

--- backend/communityapi/communityapi/settings.py
+++ backend/communityapi/communityapi/settings.py
@@ -166,7 +166,7 @@ MIDDLEWARE = [
      "django.contrib.messages.middleware.MessageMiddleware",
      "django.middleware.clickjacking.XFrameOptionsMiddleware",
      "quercus.middleware.QuercusRateLimitMiddleware",
-]
+]   
 
 ROOT_URLCONF = 'communityapi.urls'
 
